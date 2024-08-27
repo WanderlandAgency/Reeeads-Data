25d5f62a0dcd6086cd17aa20ead803132aaf65bc
Bryan.D Pro <74014416+bryandelaitre@users.noreply.github.com>
2024-08-27 08:51:53 +0200

    Fixed pagination issue

--- src/index.js
+++ src/index.js
@@ -1,38 +1,65 @@
-const items = document.querySelectorAll('[reeeads-data]')
+var items = document.querySelectorAll('[reeeads-data]')
 const itemsData = {};
+const nextButton = document.querySelector('[reeeads-button="next"]');
+const prevButton = document.querySelector('[reeeads-button="previous"]');
 
-items.forEach((item) => {
-     jsonUrl = item.getAttribute('reeeads-data');
-     let figmaButton = item.querySelector('[reeeads-button="figma"]');
-     let canvaButton = item.querySelector('[reeeads-button="canva"]');
+// INIT SCRIPT
+loadScript();
 
-     if(jsonUrl !== null && jsonUrl !== undefined) {
-          fetch(jsonUrl)
-          .then(response => response.json())
-          .then(data => {
-               console.log('success');
-               canvaButton.href = data.canva.url;
-          })
-          .catch(error => console.error(error));
-     }
-     console.log(jsonUrl);
-     figmaButton.addEventListener('click', (e) => {
-          console.log(item.getAttribute('reeeads-data'));
-          console.log('Figma button clicked');
-          fetch(item.getAttribute('reeeads-data'))
-               .then(response => response.json())
-               .then(data => {
-                    console.log(data)
-                    copyToClipboard(data.figma.text, data.figma.html);
-                    item.querySelector('.button__label').innerHTML = 'Copied!';
-                    setTimeout(() => {
-                         item.querySelector('.button__label').innerHTML = 'Copy';
-                    }, 2000);
-               })
-               .catch(error => console.error(error));
-     });
+// IF NEXT BUTTON IS CLICKED THEN RELOAD SCRIPT
+nextButton.addEventListener('click', (e) => {
+     setTimeout(() => {
+          items = document.querySelectorAll('[reeeads-data]');
+          loadScript();
+     }, 300); 
 });
 
+// IF PREVIOUS BUTTON IS CLICKED THEN RELOAD SCRIPT
+prevButton.addEventListener('click', (e) => {
+     setTimeout(() => {
+          items = document.querySelectorAll('[reeeads-data]');
+          loadScript();
+     }, 300); 
+});
+
+// LOAD SCRIPT
+async function loadScript(){
+     // GET JSON DATA
+     items.forEach((item) => {
+          jsonUrl = item.getAttribute('reeeads-data');
+          let figmaButton = item.querySelector('[reeeads-button="figma"]');
+          let canvaButton = item.querySelector('[reeeads-button="canva"]');
+     
+          // GET CANVA URL FROM JSON FILE AND ADD IT TO CANVA BUTTON
+          if(jsonUrl !== null && jsonUrl !== undefined) {
+               fetch(jsonUrl)
+               .then(response => response.json())
+               .then(data => {
+                    canvaButton.href = data.canva.url;
+               })
+               .catch(error => {
+                    // console.error(error);
+               });
+          }
+          // ADD EVENT LISTENER TO FIGMA BUTTON TO COPY FIGMA DATA TO CLIPBOARD
+          figmaButton.addEventListener('click', (e) => {
+               fetch(item.getAttribute('reeeads-data'))
+                    .then(response => response.json())
+                    .then(data => {
+                         copyToClipboard(data.figma.text, data.figma.html);
+                         item.querySelector('.button__label').innerHTML = 'Copied!';
+                         setTimeout(() => {
+                              item.querySelector('.button__label').innerHTML = 'Copy';
+                         }, 2000);
+                    })
+                    .catch(error => {
+                         // console.error(error);
+                    });
+          });
+     });
+}
+
+// COPY TO CLIPBOARD
 async function copyToClipboard(text, html) {
      const blob = new Blob([html], { type: 'text/html' });
    
@@ -43,8 +70,7 @@ async function copyToClipboard(text, html) {
            'text/html': blob
          })
        ]);
-       console.log('Text and HTML copied to clipboard');
      } catch (err) {
-       console.error('Failed to copy text and HTML: ', err);
+     //   console.error('Failed to copy text and HTML: ', err);
      }
    }
